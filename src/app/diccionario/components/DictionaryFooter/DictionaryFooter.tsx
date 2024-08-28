4baf6ca3d113ccf59476ec8e9a89dd9c972a4d4b
PauliMorales <paumor271@gmail.com>
2024-08-28 15:27:55 -0500

    Añadir color para dark mode

--- src/app/diccionario/components/DictionaryFooter/DictionaryFooter.tsx
+++ src/app/diccionario/components/DictionaryFooter/DictionaryFooter.tsx
@@ -1,6 +1,6 @@
 const DictionaryFooter = () => {
   return (
-    <div className="sticky top-full flex justify-center">Developed with love ♥ by Paula Morales</div>
+    <div className="sticky top-full flex justify-center font-semibold text-fuchsia-700 dark:text-fuchsia-400 ">Developed with love ♥ by Paula Morales</div>
   );
 };
 
