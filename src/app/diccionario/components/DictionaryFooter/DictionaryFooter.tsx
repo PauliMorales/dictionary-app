1370de03c881f11aec6d67eedab24935ca418dc7
PauliMorales <paumor271@gmail.com>
2024-08-28 14:30:08 -0500

    Componente footer

---
+++ src/app/diccionario/components/DictionaryFooter/DictionaryFooter.tsx
@@ -0,0 +1,7 @@
+const DictionaryFooter = () => {
+  return (
+    <div className="sticky top-full flex justify-center">Developed with love ♥ by Paula Morales</div>
+  );
+};
+
+export default DictionaryFooter;
